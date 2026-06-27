84db58062bc266c0f0d81c8c0e0cf8421d58ee16
Franz Király <fkiraly@gcos.ai>
2026-06-27 19:21:26 +0200

    move

--- sktime_cython/transformations/rocket/__init__.py
+++ sktime_cython/transformations/rocket/__init__.py
@@ -1,1 +1,8 @@
 """Rocket transformers."""
+
+from sktime_cython.transformations.rocket._minirocket import (
+    rocket_fit,
+    rocket_transform,
+)
+
+__all__ = ["rocket_fit", "rocket_transform"]
